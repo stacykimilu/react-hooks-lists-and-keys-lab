9fa0bb12792ec3014cd436e86e07d5ee2ed91c40
Stacy <stacykimilu@gmail.com>
2024-04-29 15:57:21 +0300

    react hooks lists and keys lab

--- src/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
-
-export default NavBar;
